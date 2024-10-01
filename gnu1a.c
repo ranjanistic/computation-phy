315262e59a1252af15b88e6bdd0daa38357b7ecf
ranjanistic <priyanshuranjan88@gmail.com>
2024-10-01 23:23:11 +0530

    init

---
+++ gnu1a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x, y, r, t, x0;
+
+    FILE *fp;
+    fp = fopen("gnu1a.txt", "w");
+
+    for (x = 0; x <= 4; x += 0.1){
+        y = 0;
+        fprintf(fp, "%f\t%f\n", x, y);
+    }
+
+    for (y = 0; y < 3; y += 0.1){
+        x = 4;
+        fprintf(fp, "%f\t%f\n", x, y);
+    }
+ 
+    for (x = 0; x < 4; x += 0.1){
+        y = (3 / 4) * x;
+        fprintf(fp, "%f\t%f\n", x, y);
+    }
+    return 0;   
+}
---
+++ gnu1b.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+#define pi 3.14
+
+int main(){
+    float x, y, t;
+
+    FILE *fp = NULL;
+
+    fp = fopen("gnu1b.txt", "w");
+
+    float x0 = 5, y0 = 5, r = 3;
+
+    for(float t = 0; t<=2*pi; t+=pi/100.0){
+        y = y0 + r*sin(t);
+        x = x0 + r*cos(t);
+        fprintf(fp, "%f\t%f\n", x, y);
+    }
+    return 0;
+}
