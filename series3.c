6df4c53957391fc659958b52ba64e20d06199fb0
ranjanistic <priyanshuranjan88@gmail.com>
2024-10-03 21:32:23 +0530

    fix series3

--- series3.c
+++ series3.c
@@ -3,10 +3,20 @@
 
 #define pi 3.14159
 
+int fact(int n){
+    if(n == 0) return 1;
+    int i = n;
+    while((i - 1) > 0){
+        i*=(i - 1);
+        i--;
+    }
+    return i;
+}
+
 float j(int n, float x, float acc){
-    float s = 0, t = 1, k = 0;
+    float s = 1/fact(n), t = 1, k = 1;
     do{
-        t*=-(pow(x,2)/4)/(k+1)*(n+k+1);
+        t*=-(pow(x,2)/4)/((k)*(n+k));
         s+=t;
         k++;
     } while(fabs(t/s) > acc);
@@ -17,9 +27,10 @@ float j(int n, float x, float acc){
 
 int main(){
     FILE *fp = fopen("data/series3.dat", "w");
-        for(float x = 0; x <= 10; x+=0.01){
+        for(float x = 0; x <= 16; x+=0.01){
             fprintf(fp, "%f\t%f\t%f\t%f\n", x, j(0, x, 0.001),j(1, x, 0.001),j(2, x, 0.001));
         }
     
     return 0;
 }
+
